977c9a8146a0f9213411818b642099098649f9a0
Adam Nguyen <66979547+Adanato@users.noreply.github.com>
2024-06-12 14:35:58 -0500

    fixed errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
-import About from './pages/About';
-import ArenaBattle from './pages/ArenaBattle';
 import Home from './pages/Home';
-import Leaderboard from './pages/Leaderboard';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 function App() {
